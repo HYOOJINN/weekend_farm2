e4334a46ca2531640e34a137729974ce29c2d1d3
HYOOJINN <yhjhj23@gmail.com>
2019-11-13 23:01:41 +0900

    현재위치 완성(?) - maps가 아니라 main에 구현함. 농장 위치 야매로 2개 찍어놓음

--- app/src/main/java/com/example/buyer_map/HomeActivity.java
+++ app/src/main/java/com/example/buyer_map/HomeActivity.java
@@ -1,11 +1,11 @@
 package com.example.buyer_map;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -21,10 +21,10 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void ClickButton2(View v){
-///잘됨진짜
+
 
         //지도가 잘 안됨
-        Intent intent=new Intent(getApplicationContext(),MapsActivity.class);
+        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
         startActivity(intent);
         Toast.makeText(getApplicationContext(),"구매자 버튼을 눌렀습니다",Toast.LENGTH_LONG).show();
     }

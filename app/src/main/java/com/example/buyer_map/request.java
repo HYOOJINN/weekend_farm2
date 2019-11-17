c72fc9bc1d3504f965f9fe1ecf8df27fdb557edb
HYOOJINN <yhjhj23@gmail.com>
2019-11-17 17:15:16 +0900

    request activity 수정

--- app/src/main/java/com/example/buyer_map/request.java
+++ app/src/main/java/com/example/buyer_map/request.java
@@ -1,6 +1,8 @@
 package com.example.buyer_map;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,4 +13,13 @@ public class request extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
     }
+
+
+    public void ClickButton1(View v){
+        Toast.makeText(getApplicationContext(),"요청을 완료했습니다.",Toast.LENGTH_LONG).show();
+    }
+
+
+
+
 }
